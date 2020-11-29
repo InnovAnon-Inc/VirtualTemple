5045914d802fa1441d9e0d306efd42acb7f9aecb
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-29 02:13:52 -0600

    dev ops

--- public/events.js
+++
@@ -1,4 +0,0 @@
-openButton.addEventListener("click", openButtonFunc);
-createButton.addEventListener("click", createButtonFunc);
-joinButton.addEventListener("click", joinButtonFunc);
-hangupButton.addEventListener("click", hangupButtonFunc);
