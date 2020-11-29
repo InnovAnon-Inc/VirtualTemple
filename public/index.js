dbe84758a43b5fa9756e5f2750991d4f73458744
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-29 01:26:43 -0600

    dev ops

--- public/index.js
+++ public/index.js
@@ -4,27 +4,28 @@ let roomId;
 let roomIdData = [];
 let peerConnection;
 
-const freeice = require('freeice');
+// firebase doesn't support require(), apparently
+//const freeice = require('freeice');
 
 let configuration = {
-    /*iceServers: [
+    iceServers: [
       {urls: 'stun:stun.services.mozilla.com'},
       {urls: ['stun:stun.l.google.com:19302',
-              'stun:stun1.l.google.com:19302',
-              'stun:stun2.l.google.com:19302',
-              'stun:stun3.l.google.com:19302',
-              'stun:stun4.l.google.com:19302',]},
-      {urls: 'stun:stun.ekiga.net'},
-      {urls: 'stun:stun.ideasip.com'},
-      {urls: 'stun:stun.rixtelecom.se'},
-      {urls: 'stun:stun.schlund.de'},
-      {urls: 'stun:stun.stunprotocol.org:3478'},
-      {urls: 'stun:stun.voiparound.com'},
-      {urls: 'stun:stun.voipbuster.com'},
-      {urls: 'stun:stun.voipstunt.com'},
-      {urls: 'stun:stun.voxgratia.org'},
-    ],*/
-    iceServers: freeice({stun: 2}),
+              //'stun:stun1.l.google.com:19302',
+              //'stun:stun2.l.google.com:19302',
+              //'stun:stun3.l.google.com:19302',
+              //'stun:stun4.l.google.com:19302',]},
+      //{urls: 'stun:stun.ekiga.net'},
+      //{urls: 'stun:stun.ideasip.com'},
+      //{urls: 'stun:stun.rixtelecom.se'},
+      //{urls: 'stun:stun.schlund.de'},
+      //{urls: 'stun:stun.stunprotocol.org:3478'},
+      //{urls: 'stun:stun.voiparound.com'},
+      //{urls: 'stun:stun.voipbuster.com'},
+      //{urls: 'stun:stun.voipstunt.com'},
+      //{urls: 'stun:stun.voxgratia.org'},
+    ],
+    //iceServers: freeice({stun: 2}),
     iceCandidatePoolSize: 10,
   }
 
