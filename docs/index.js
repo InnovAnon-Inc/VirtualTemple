9e998dc760b613a685f69fc8b790a1e93ec818e9
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-29 03:13:00 -0600

    dev ops

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,10 @@
 
 const options = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
-        muc: 'conference.jitsi-meet.example.com' // FIXME: use XEP-0030
+        domain: 'meet.jit.si',
+        muc: 'conference.meet.jit.si' // FIXME: use XEP-0030
     },
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//meet.jit.si/http-bind', // FIXME: use xep-0156 for that
 
     // The name of client node advertised in XEP-0115 'c' stanza
     clientNode: 'http://jitsi.org/jitsimeet'
