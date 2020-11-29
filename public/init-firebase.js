83683d11e42f3f0fa79368322f35721bec8ade2b
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-28 23:53:43 -0600

    update

--- public/init-firebase.js
+++ public/init-firebase.js
@@ -1,15 +1,17 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-	apiKey: "AIzaSyDamvQgNL8UpubL3HTsW14bExCIGtTyePY",
-	authDomain: "virtual-temple.firebaseapp.com",
-	databaseURL: "https://virtual-temple.firebaseio.com",
-	projectId: "virtual-temple",
-	storageBucket: "virtual-temple.appspot.com",
-	messagingSenderId: "799663626723",
-	appId: "1:799663626723:web:7307f8741f0caf3918a8d6",
-	measurementId: ""
+  "projectId": "virtual-temple",
+  "appId": "1:799663626723:web:7307f8741f0caf3918a8d6",
+  "databaseURL": "https://virtual-temple.firebaseio.com",
+  "storageBucket": "virtual-temple.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDamvQgNL8UpubL3HTsW14bExCIGtTyePY",
+  "authDomain": "virtual-temple.firebaseapp.com",
+  "messagingSenderId": "799663626723",
+  "measurementId": "G-031TE51FGB"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
